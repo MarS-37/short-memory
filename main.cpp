ba6d9ebd2d9e4e9301476dba6efa23f362e5862c
Mars-37 <master_sbo@mail.ru>
2024-05-28 20:38:11 +0300

    variables

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
-
+#include <chrono>
+#include <ctime>
 
 int main()
 {
+	// srand initialization
+	std::srand(std::time(nullptr));
 
+	// random string length
+	int length_line; 
+
+
+
+	return 0;
 }
