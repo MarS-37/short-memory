1cd756b7fb3a3e1860d408cc539152c38c02ade8
Mars-37 <master_sbo@mail.ru>
2024-05-28 21:33:03 +0300

    enhancement generateRandomString

--- main.cpp
+++ main.cpp
@@ -32,11 +32,12 @@ void saveLengthToFile(int length_line) {
 }
 
 
-std::string generateRandomString(int length) {
+std::string generateRandomString(int length, const std::string& charset) {
 	std::string randomString;
+	int charsetSize = charset.size();
 
 	for (int i = 0; i < length; ++i) {
-		char randomChar = 'a' + std::rand() % 26;
+		char randomChar = charset[std::rand() % charsetSize];
 		randomString += randomChar;
 	}
 
@@ -56,13 +57,14 @@ int main()
 	std::srand(std::time(nullptr));
 	// random string length
 	int length_line; 
+	std::string charset = "AdNbGtRe%369:?\";";
 
 	loadLengthFromFile(length_line);
 
 	while (true) {
-		std::string line = generateRandomString(length_line);
+		std::string line = generateRandomString(length_line, charset);
 		std::cout << line << std::endl;
-
+		
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>((length_line * 1.5 + length_line) * 1000)));
 
