2b6141ccf414179f2d6973069b75e06c8455b9d8
Mars-37 <master_sbo@mail.ru>
2024-05-28 20:44:00 +0300

    loadLengthFromFile

--- main.cpp
+++ main.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
+#include <fstream>
 #include <chrono>
 #include <ctime>
+
+
+void loadLengthFromFile(int& length_line)
+{
+	std::ifstream settingsFile("settings.txt");
+
+	if (settingsFile.is_open()) {
+		settingsFile >> length_line;
+		settingsFile.close();
+	}
+	else {
+		// default value if the file doesn't exist
+		length_line = 1;
+	}
+}
+
 
 int main()
 {
 	// srand initialization
 	std::srand(std::time(nullptr));
-
 	// random string length
 	int length_line; 
 
+	loadLengthFromFile(length_line);
+
+	while (false) {
+
+	}
 
 
 	return 0;
